ffbf6233f42933bd10ca4223198ca086ae7b7aac
Maximiliano Bury <88646230+maxinbury@users.noreply.github.com>
2022-08-03 10:46:08 -0300

    se arregla OTES

--- src/services/lotes.js
+++ src/services/lotes.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 
-//const  baseUrl ='http://52.90.82.154:4000/otes/'
-const  baseUrl ='http://localhost:4000/lotes/'
+const  baseUrl ='http://52.90.82.154:4000/lotes/'
+// const  baseUrl ='http://localhost:4000/lotes/'
 
 
 const lista= async  () => {
