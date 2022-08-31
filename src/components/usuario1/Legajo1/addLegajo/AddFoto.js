e3520b9da8bfeaf73f7b518d31fa933b6aa5a0f8
Pipo <pipao.pipo@gmail.com>
2022-08-31 00:31:22 -0300

    correciciones

--- src/components/usuario1/Legajo1/addLegajo/AddFoto.js
+++ src/components/usuario1/Legajo1/addLegajo/AddFoto.js
@@ -11,25 +11,33 @@ const AddFoto = (props) => {
     console.log('click');
   };                             
   const [file, setFile] = useState(null);
+  const [fileUpload, setFileUpload] = useState(null);
 
-
-  const onDrop = useCallback((acceptedFiles) => {
-    setFile(acceptedFiles);
-   
+  const onDrop = useCallback  ((files, acceptedFiles) => {
+    const formData = new FormData();
+    setFileUpload(acceptedFiles);
+    formData.append('file', files[0]);
   
-  }, []);
-  const { getRootProps, getInputProps, isDragActive, isDragAccept, acceptedFiles } = useDropzone({
-    onDrop,
-    multiple: true,
-    accept: 'document/*',
-    maxFiles: 3,
+    formData.append('cuil_cuit', props.cuil_cuit);
+   
+     servicioLegajo.subirlegajo1(formData)
+       // window.location.reload(true);
+     
 
-  });
-  const acceptedFileItems = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-    </li>
-  ));
+
+    });
+    const { getRootProps, getInputProps, isDragActive, isDragAccept, acceptedFiles } = useDropzone({
+      onDrop,
+      multiple: false,
+      accept: 'document/*',
+  
+    });
+
+    const acceptedFileItems = acceptedFiles.map(file => (
+      <li key={file.path}>
+        {file.path} - {file.size} bytes
+      </li>
+    )); 
   const handleChange = (e) => {
     setFile({ ...file, [e.target.name]: e.target.value })
     
@@ -39,42 +47,45 @@ const AddFoto = (props) => {
    console.log(file)
   }
 
-  const enviar = () =>{
-   if (!file){
-    alert('No seleccionaste el archivo')
-    return
-   }
-   let formdata = new FormData()
-   console.log(file)
-   formdata.append('image', file)
-   formdata.append('tipo','dni')
-   formdata.append('cuil_cuit', props.cuil_cuit)
- 
-   console.log(formdata)
-   servicioLegajo.subirprueba(formdata)
+  const enviar = () => {
+    if (!file) {
+        alert('No seleccionaste el archivo')
+        return
 
-   }
+    }
+    let formdata = new FormData()
+    console.log(file)
+    formdata.append('image', file)
+
+
+
+
+
+
+    servicioLegajo.subirlegajode(formdata)
+    window.location.reload(true);
+}
   return (
     <>
-      <Paper
-        sx={{
-          cursor: 'pointer',
-          background: '#fafafa',
-          color: '#bdbdbd',
-          border: '1px dashed #ccc',
-          '&:hover': { border: '1px solid #ccc' },
-        }}
-      >
-        <div style={{ padding: '16px' }} {...getRootProps()}>
-          <input {...getInputProps()} onChange={handleChange}/>
-          {isDragActive ? (
-            <p style={{ color: 'green' }}>Suelta aqui la foto</p>
-          ) : (
-            <p>Arrastra hasta aqui la foto de tu DNI</p>
-          )}
-          <em>(images with *.jpeg, *.png, *.jpg extenciones aceptadas)</em>
-        </div>
-      </Paper>
+       <Paper
+          sx={{
+            cursor: 'pointer',
+            background: '#fafafa',
+            color: '#bdbdbd',
+            border: '1px dashed #ccc',
+            '&:hover': { border: '1px solid #ccc' },
+          }}
+        >
+          <div style={{ padding: '16px' }} {...getRootProps()}>
+            <input {...getInputProps()} />
+            {isDragActive ? (
+              <p style={{ color: 'green' }}>Suelta aqui el documento</p>
+            ) : (
+              <p>Arrastra hasta aqui el archivo descargado con tus datos personales</p>
+            )}
+            <em>(Documentos .*pdf, .*doc, *.jpeg, *.png, *.jpg  extenciones aceptadas)</em>
+          </div>
+        </Paper>
       <Box sx={{ m: 1, 
       color: 'green',
       fontSize: '1rem',      }}
