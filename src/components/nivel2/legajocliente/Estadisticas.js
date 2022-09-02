75195d19f7aa29e16f1e2e5f9c1fab528e6611f3
Pipo <pipao.pipo@gmail.com>
2022-09-02 18:37:08 -0300

    bugs legajos

--- src/components/nivel2/legajocliente/Estadisticas.js
+++ src/components/nivel2/legajocliente/Estadisticas.js
@@ -18,7 +18,7 @@ import { useNavigate } from "react-router-dom";
 import MUIDataTable from "mui-datatables";
 import EditIcon from "@material-ui/icons/Edit";
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import Snackbar from '@mui/material/Snackbar';
 
 
 
@@ -28,122 +28,144 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Legajos(props) {
 
-    const [cli, setCli] = useState(
-        {
-            cuil_cuit:props.cuil_cuit
-        });
-    const [datos, setDatos] = useState(null);
-    const [historial, setHistorial] = useState([]);
-    const navigate = useNavigate();
-    useEffect(() => {
-  
-      buscar()
-  
-    }, [])
-  
+  const [cli, setCli] = useState(
+    {
+      cuil_cuit: props.cuil_cuit
+    });
+  const [datos, setDatos] = useState(null);
+  const [historial, setHistorial] = useState([]);
+  const navigate = useNavigate();
+  const [state, setState] = React.useState({
+    open: false,
+    vertical: 'bottom',
+    horizontal: 'right',
+  });
+  const { vertical, horizontal, open } = state;
+  useEffect(() => {
 
-  
-  
-  
-    const borrar = (e) => {
-  
+    buscar()
+
+  }, [])
+
+
+
+
+
+  const borrar = (e) => {
+
     console.log(e)
-    }
-  
-    const buscar = async () => {
-  
-      const datoss = await servicioClientes.datoslegajo(cli)
-      setDatos(datoss)
-      //setHistorial(datos[2])
-  
-    };
-  
-    
-     
-      
-     
-  
-  
-    return (
-        <div>
-   
-
-  
-  
-        {datos ?
-          <div>
-            <div className="home">
-  
-              <div className="container">
-  
-                <div className="widgets">
-                {datos ?
-                    <div>
-                      <Widget type="total"
-                         cantidad={datos[0].total} 
-                        />
-                    </div> : <div> <Widget type="total"
-                    /></div>
-                  }
-                 
-                  {datos ?
-                    <div>
-                      <Widget type="Pendientes"
-                         cantidad={datos[0].Pendientes } 
-                         porcentaje={datos[0].porcPendientes}
-                        />
-                    </div> : <div> <Widget type="Pendientes"
-                    /></div>
-                  }
-                    {datos ?
-                    <div>
-                      <Widget type="Aprobadas"
-                         cantidad={datos[0].Aprobadas} 
-                         porcentaje={datos[0].porcAprobadas}
-                        />
-                    </div> : <div> <Widget type="Aprobadas"
-                    /></div>
-                  }
-                  {datos ?
-                    <div>
-                      <Widget type="Rechazadas"
-                         cantidad={datos[0].Rechazadas} 
-                         porcentaje={datos[0].porcRechazadas}
-                         
-                        />
-                    </div> : <div> <Widget type="Rechazadas"
-                    /></div>
-                  }
-                 
-                
-                </div>
-                <div className="charts">
-                    
-               {/*  {datos ?
-                    <div>
-                      <Featured 
-                       porcentaje={datos[0].porcDenuncia}
-                       titulo="Con Denuncia"
-                       
-                      />
-                      </div> : <div><Featured /> </div> }
-                   */}
-                    
-  
-         
-                </div>
-             
-              </div>
-            </div>
-  
-  
-          </div>
-  
-          : <div></div>}
-  
-  </div>
-    );
-  
   }
-  
-  
+
+  const buscar = async () => {
+
+    const datoss = await servicioClientes.datoslegajo(cli)
+    setDatos(datoss)
+    //setHistorial(datos[2])  
+    setState({ open: true, ...{ vertical: 'top', horizontal: 'right' } })
+
+  };
+
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
+
+
+
+
+
+  return (
+    <div>
+      {datos ?
+        <div>
+          <Snackbar
+            anchorOrigin={{ vertical, horizontal }}
+            open={open}
+             onClose={handleClose}
+            message={datos[0].Faltan}
+            key={vertical + horizontal}
+          />
+        </div> : <div> </div>
+      }
+      {datos ?
+        <div>
+          <Featured
+            porcentaje={datos[0].porccompleto}
+            titulo="Completo"
+
+          />
+        </div> : <div><Featured /> </div>}
+
+
+
+      {datos ?
+        <div>
+          <div className="home">
+
+            <div className="container">
+
+              <div className="widgets">
+                {datos ?
+                  <div>
+                    <Widget type="total"
+                      cantidad={datos[0].total}
+                    />
+                  </div> : <div> <Widget type="total"
+                  /></div>
+                }
+
+                {datos ?
+                  <div>
+                    <Widget type="Pendientes"
+                      cantidad={datos[0].Pendientes}
+                      porcentaje={datos[0].porcPendientes}
+                    />
+                  </div> : <div> <Widget type="Pendientes"
+                  /></div>
+                }
+                {datos ?
+                  <div>
+                    <Widget type="Aprobadas"
+                      cantidad={datos[0].Aprobadas}
+                      porcentaje={datos[0].porcAprobadas}
+                    />
+                  </div> : <div> <Widget type="Aprobadas"
+                  /></div>
+                }
+                {datos ?
+                  <div>
+                    <Widget type="Rechazadas"
+                      cantidad={datos[0].Rechazadas}
+                      porcentaje={datos[0].porcRechazadas}
+
+                    />
+                  </div> : <div> <Widget type="Rechazadas"
+                  /></div>
+                }
+
+
+
+
+
+              </div>
+              <div className="charts">
+
+
+
+
+
+
+              </div>
+
+            </div>
+          </div>
+
+
+        </div>
+
+        : <div></div>}
+
+    </div>
+  );
+
+}
+
