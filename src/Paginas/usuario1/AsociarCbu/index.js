3c2ec5a7c560e2d132aa89b4f20fa262f528e572
Pipo <pipao.pipo@gmail.com>
2022-09-04 00:33:09 -0300

    asociar cbu

---
+++ src/Paginas/usuario1/AsociarCbu/index.js
@@ -0,0 +1,32 @@
+
+
+
+import Navbar from '../../../components/usuario1/Navbar1'
+import Modalasociar from '../../../components/usuario1/AsociarCbu/ModalAsociar'
+
+
+
+
+
+
+export default function asociarCbu() {
+  
+
+    return (
+        <>
+            <div className="App">
+
+
+                <Navbar />
+                <br />  <br />  <br />  <br />
+                < Modalasociar/>
+             
+                
+
+            </div>
+
+
+        </>
+    );
+
+}
--- src/components/usuario1/cards/CardCuatro.js
+++ src/components/usuario1/cards/CardCuatro.js
@@ -1,15 +1,18 @@
 import * as React from 'react';
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import "./CardStyle.css";
+import { useNavigate } from 'react-router-dom';
 const CardCuatro = () => {
-
-
+    const navigate=useNavigate()
+    const ir = () => {
+        navigate('/usuario/asociarcbu')
+    }
     return (
         <div className="body__Page">
             <div className="container__article">
     
                 <div className="box__article">
-                <i>< AccountBalanceIcon /> </i>
+                <i onClick={ir}>< AccountBalanceIcon /> </i>
                     <h5>Asocia tu CBU</h5>
                 </div>
                
