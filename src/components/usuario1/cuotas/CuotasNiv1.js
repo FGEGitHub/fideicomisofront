ef3b2f0bcb602af4cb8d83c43e1bdbfe74eb431f
Pipo <pipao.pipo@gmail.com>
2023-01-09 12:56:30 -0300

    visualizacion cuotas niv1

--- src/components/usuario1/cuotas/CuotasNiv1.js
+++ src/components/usuario1/cuotas/CuotasNiv1.js
@@ -1,28 +1,46 @@
-import { useParams } from "react-router-dom"
+import Grid from '@mui/material/Grid';
 import servicioUsuario1 from '../../../services/usuario1'
 import servicioCuotas from '../../../services/cuotas'
 import React, { useEffect, useState, Fragment } from "react";
-import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
-import SearchIcon from '@mui/icons-material/Search';
-import DeleteIcon from '@mui/icons-material/Delete';
+import Skeleton from '@mui/material/Skeleton';
 import Fab from '@mui/material/Fab';
 import LocalAtmIcon from '@mui/icons-material/LocalAtm';
 import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
 import Button from '@mui/material/Button';
-import MUIDataTable from "mui-datatables";
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import TextField from '@mui/material/TextField';
 import { useNavigate } from "react-router-dom";
-import UseUser from '../../../hooks/useUser'
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import Stack from '@mui/material/Stack';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
+import { styled } from '@mui/material/styles';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Box } from "@material-ui/core";
 
 
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 
 
@@ -34,6 +52,10 @@ const CuotasNiv1 = (props) => {
 
   const [act, setAct] = useState(false)
   const [act2, setAct2] = useState(false)
+
+  const [verPag, setVerpag] = useState(false)
+  const [verCuot, setVerCuot] = useState(false)
+
 
   const [cuotas, setCuotas] = useState([''])
   const [pagos, setPagos] = useState([''])
@@ -79,12 +101,13 @@ const CuotasNiv1 = (props) => {
   };
 
   const vercuotas = async (index) => {
-    console.log('ver cuotas')
+    
     const cuotas = await servicioUsuario1.vercuotas(index)
     console.log(cuotas)
     if (cuotas[0] !== '') {
-       setCuotas(cuotas[0]) 
-       setPagos(cuotas[1])}
+      setCuotas(cuotas[0])
+      setPagos(cuotas[1])
+    }
     setAct(true)
 
   };
@@ -118,6 +141,13 @@ const CuotasNiv1 = (props) => {
     alert(rta)
 
   }
+  
+
+  const ocultarIEF = () => {
+    console.log('etc')
+    setAct2(false)
+  };
+ 
 
 
 
@@ -166,73 +196,73 @@ const CuotasNiv1 = (props) => {
 
   function fecha(dataIndex, rowIndex, data, onClick) {
     return (
-        <>
-            { pagos[dataIndex].mes + "/"+ pagos[dataIndex].anio }
+      <>
+        {pagos[dataIndex].mes + "/" + pagos[dataIndex].anio}
 
-        </>
+      </>
     );
-}
+  }
 
-function monnto(dataIndex, rowIndex, data, onClick) {
-  return (
+  function monnto(dataIndex, rowIndex, data, onClick) {
+    return (
       <>
-          { "$ "+ pagos[dataIndex].monto }
+        {"$ " + pagos[dataIndex].monto}
 
       </>
-  );
-}
+    );
+  }
 
-function estadoo(dataIndex, rowIndex, data, onClick) {
-  return (
+  function estadoo(dataIndex, rowIndex, data, onClick) {
+    return (
       <>
-          {  pagos[dataIndex].estado === "A" ? <>Aprobado</>: <> Pendiente/No Aprobado</> }
+        {pagos[dataIndex].estado === "A" ? <>Aprobado</> : <> Pendiente/No Aprobado</>}
 
       </>
-  );
-}
+    );
+  }
   const columnaspagos = [
     {
       name: "fecha",
       options: {
-          customBodyRenderLite: (dataIndex, rowIndex) =>
+        customBodyRenderLite: (dataIndex, rowIndex) =>
           fecha(
-                  dataIndex,
-                  rowIndex,
-                  // overbookingData,
-                  // handleEditOpen
-              )
+            dataIndex,
+            rowIndex,
+            // overbookingData,
+            // handleEditOpen
+          )
       }
 
-  },
+    },
 
     {
       name: "Estado",
       options: {
-          customBodyRenderLite: (dataIndex, rowIndex) =>
+        customBodyRenderLite: (dataIndex, rowIndex) =>
           estadoo(
-                  dataIndex,
-                  rowIndex,
-                  // overbookingData,
-                  // handleEditOpen
-              )
+            dataIndex,
+            rowIndex,
+            // overbookingData,
+            // handleEditOpen
+          )
       }
 
-  },
-  {
-    name: "Monto",
-    options: {
+    },
+    {
+      name: "Monto",
+      options: {
         customBodyRenderLite: (dataIndex, rowIndex) =>
-        monnto(
-                dataIndex,
-                rowIndex,
-                // overbookingData,
-                // handleEditOpen
-            )
-    }
+          monnto(
+            dataIndex,
+            rowIndex,
+            // overbookingData,
+            // handleEditOpen
+          )
+      }
 
-},
+    },
 
-    
+
 
 
 
@@ -242,128 +272,262 @@ function estadoo(dataIndex, rowIndex, data, onClick) {
 
     <Fragment>
 
-      <br /><br /><br /><br /><br /><br /><br /><br />
-
+      <br /><br /><br /><br /><br /><br />
+    
       {
         lotes.map((item, index) =>
-        <Stack spacing={2} direction="row">
+          <Stack spacing={2} direction="row">
             <Fab key={index} variant="extended" onClick={() => { vercuotas(item['id']) }}><AddLocationAltIcon sx={{ mr: 1 }} /> {item['zona']} Manzana {item['manzana']} Parcela {item['parcela']}
-            
+
             </Fab>
             {/*  <Button  key= {index} variant="contained"onClick={()=>{agregar(item['id'])}}> Agregar Cuotas</Button> */}
             <Fab key={index} variant="extended" onClick={() => { verief(item['id']) }}>
-            <LocalAtmIcon sx={{ mr: 1 }} />
-            Estado financiero
+              <LocalAtmIcon sx={{ mr: 1 }} />
+              Estado financiero
             </Fab>
+            <br/>
 
             {/* <Button key={index} variant="contained" onClick={() => { verief(item['id']) }}> Estado financiero </Button> */}
 
 
 
-
+        
+        
           </Stack>
         )
       }
 
+      {act2 ?
+        <div>
+            <Box
+            sx= {{
+              display: 'flex'
+            }}
+            >
+ <Grid container  columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+
+          <Paper
+            sx={{
+              cursor: 'pointer',
+              background: '#eeeeee',
+              color: '#bdbdbd',
+              border: '1px dashed #ccc',
+              width: "40%",
+              '&:hover': { border: '1px solid #ccc' },
+              border: "1px solid black",
+              margin: '75px',
+              display: 'flex'
+
+            }}
+          >
+
+            <TableContainer >
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Detalles de Deuda Exigible </TableCell>
+
+
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {deudaExigible.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+
+                      <TableCell align="left">{row.datoa}</TableCell>
+                      <TableCell align="left">{row.datob}</TableCell>
+
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+
+
+          <Paper
+            sx={{
+              cursor: 'pointer',
+              background: '#eeeeee',
+              color: '#bdbdbd',
+              border: '1px dashed #ccc',
+              width: "40%",
+              '&:hover': { border: '1px solid #ccc' },
+              border: "1px solid black",
+              margin: '75px',
+              display: 'flex'
+
+            }}
+          >
+
+            <TableContainer >
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Detalle de Cuotas Pendientes </TableCell>
+
+
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {detallePendiente.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+
+                      <TableCell align="left">{row.datoa}</TableCell>
+                      <TableCell align="left">{row.datob}</TableCell>
+
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+       
+          <Fab  sx={{ margin: '75px',}} variant="extended" onClick={() => { ocultarIEF() }}  ><VisibilityOffIcon sx={{ mr: 1 }}  /> Ocultar IEF</Fab>
+          </Grid>
+          </Box>
+        </div>
+        : <div></div>}
+
       {act ?
         <div>
+
+          <>
+            <Paper
+              sx={{
+                cursor: 'pointer',
+                background: '#eeeeee',
+                color: '#bdbdbd',
+                border: '1px dashed #ccc',
+                width: "90%",
+                '&:hover': { border: '1px solid #ccc' },
+                border: "1px solid black",
+                margin: '75px',
+
+              }}
+            >
+
+              <TableContainer>
+                {!cuotas ? <Skeleton /> : <>
+                  <h1>CUOTAS</h1>
+                  <Table >
+                    <TableHead>
+                      <TableRow>
+                        <TableCell style={{ backgroundColor: "black", color: 'white' }} ><b>FECHA</b> <b /></TableCell>
+                        <TableCell style={{ backgroundColor: "black", color: 'white' }}><b>SALDO INICIAL</b></TableCell>
+                        <TableCell style={{ backgroundColor: "black", color: 'white' }}><b>ICC</b></TableCell>
+                        <TableCell style={{ backgroundColor: "black", color: 'white' }} ><b>CUOTA CON AJUSTE</b></TableCell>
+                        <TableCell style={{ backgroundColor: "black", color: 'white' }} ><b>AJUSTE ICC</b></TableCell>
+                        <TableCell style={{ backgroundColor: "black", color: 'white' }} ><b>SALDO REAL</b></TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+
+
+
+                      {cuotas.map((row) => (
+                        <StyledTableRow key={row.name}>
+                          <StyledTableCell component="th" scope="row">{row.mes}/{row.anio} </StyledTableCell>
+                          <StyledTableCell component="th" scope="row">$ {row.saldo_inicial} </StyledTableCell>
+                          <StyledTableCell component="th" scope="row">{row.ICC} </StyledTableCell>
+                          <StyledTableCell component="th" scope="row">$ {row.cuota_con_ajuste} </StyledTableCell>
+                          <StyledTableCell component="th" scope="row">{row.Ajuste_ICC} </StyledTableCell>
+                          <StyledTableCell component="th" scope="row">$ {row.Saldo_real} </StyledTableCell>
+                        </StyledTableRow>
+                      ))}
+
+
+
+
+                    </TableBody>
+                  </Table>
+                </>}
+
+              </TableContainer>
+            </Paper>
+
+
+          </>
+
+
+
+
+
           <div>
 
-            <MUIDataTable
-              title={"Lista de Cuotas"}
-              data={cuotas}
-              columns={columns}
-              actions={[
-                {
-                  icon: 'save',
-                  tooltip: 'Save User',
-                  onClick: (event, rowData) => alert("You saved " + rowData.name)
-                }
-              ]}
+            <>
+
+              <>
+                <Paper
+                  sx={{
+                    cursor: 'pointer',
+                    background: '#eeeeee',
+                    color: '#bdbdbd',
+                    border: '1px dashed #ccc',
+                    width: "90%",
+                    '&:hover': { border: '1px solid #ccc' },
+                    border: "1px solid black",
+                    margin: '75px',
+
+                  }}
+                >
+
+                  <TableContainer>
+                    <h1>PAGOS</h1>
+                    {!pagos ? <Skeleton /> : <>
+                      <Table >
+                        <TableHead>
+                          <TableRow>
+                            <TableCell style={{ backgroundColor: "black", color: 'white' }} ><b>FECHA</b> <b /></TableCell>
+                            <TableCell style={{ backgroundColor: "black", color: 'white' }}><b>ESTADO</b></TableCell>
+                            <TableCell style={{ backgroundColor: "black", color: 'white' }}><b>MONTO</b></TableCell>
+
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
 
 
 
-            />
-          </div>
-          <div>
+                          {pagos.map((row) => (
+                            <StyledTableRow key={row.name}>
+                              <StyledTableCell component="th" scope="row">{row.mes}/{row.anio} </StyledTableCell>
+                              <StyledTableCell component="th" scope="row"> {row.estado === "A" ? <>Aprobado</> : <> Pendiente/No Aprobado</>} </StyledTableCell>
+                              <StyledTableCell component="th" scope="row">{row.monto} </StyledTableCell>
 
-            <MUIDataTable
-              title={"Lista de Pagos"}
-              data={pagos}
-              columns={columnaspagos}
-              actions={[
-                {
-                  icon: 'save',
-                  tooltip: 'Save User',
-                  onClick: (event, rowData) => alert("You saved " + rowData.name)
-                }
-              ]}
+                            </StyledTableRow>
+                          ))}
 
 
 
-            />
+
+                        </TableBody>
+                      </Table>
+                    </>}
+
+                  </TableContainer>
+                </Paper>
+
+
+              </>
+
+
+
+            </>
+
+
           </div>
         </div>
         : <div></div>}
 
       <br /><br /><br /><br />
 
-      {act2 ?
-        <div>
-          <TableContainer style={{ width: '70%' }} component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Detalles de Deuda Exigible </TableCell>
 
-
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {deudaExigible.map((row) => (
-                  <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-
-                    <TableCell align="left">{row.datoa}</TableCell>
-                    <TableCell align="left">{row.datob}</TableCell>
-
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-
-          <br /><br />
-
-
-          <TableContainer style={{ width: '70%' }} component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Detalle de Cuotas Pendientes </TableCell>
-
-
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {detallePendiente.map((row) => (
-                  <TableRow
-                    key={row.name}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-
-                    <TableCell align="left">{row.datoa}</TableCell>
-                    <TableCell align="left">{row.datob}</TableCell>
-
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-        : <div></div>}
     </Fragment>
 
   )
