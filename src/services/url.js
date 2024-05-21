5b1774cfb520d8d5545fb2f56298896c7d711e2f
FGEGitHub <pipao.pipo@gmail.com>
2024-05-21 12:26:54 -0300

    https

--- src/services/url.js
+++ src/services/url.js
@@ -3,8 +3,12 @@ module.exports = {
 
 
 // database:'http://localhost:4000/'
-database: 'http://172.25.50.45:4000/'
- //database:'https://api.santacatalinafideicomiso.com/'
+database: 'https://172.25.50.45:4000/'
+
+
+
+
+//database:'https://api.santacatalinafideicomiso.com/'
 
 }
 ///const  baseUrl = '90.57.233.213:8084/'
