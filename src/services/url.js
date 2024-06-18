c6aa49f916cf1e28cb63505c821179c9d7f31f6c
FGEGitHub <pipao.pipo@gmail.com>
2024-06-17 22:02:15 -0300

    url

--- src/services/url.js
+++ src/services/url.js
@@ -2,7 +2,7 @@
 module.exports = {
 
 
-// database:'http://localhost:4000/'
+ //database:'http://localhost:4000/'
 //database: 'https://172.25.50.45:4000/'
  database:'https://apifdd.ciudaddecorrientes.gov.ar/'
 
