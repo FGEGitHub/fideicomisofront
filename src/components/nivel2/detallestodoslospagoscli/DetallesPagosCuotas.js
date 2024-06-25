f1160305030cfe102f0ff602b8e74972352c8118
FGEGitHub <pipao.pipo@gmail.com>
2024-06-25 11:18:11 -0300

    detalle

--- src/components/nivel2/detallestodoslospagoscli/DetallesPagosCuotas.js
+++ src/components/nivel2/detallestodoslospagoscli/DetallesPagosCuotas.js
@@ -1,161 +1,81 @@
 import * as React from 'react';
-import { useParams } from "react-router-dom"
-import MUIDataTable from "mui-datatables";
-import {  useState, useEffect } from "react";
-import servicioPagos from '../../../services/pagos'
-import serviciousuario1 from '../../../services/usuario1'
-import Borrar from './modalborrar';
-import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import servicioPagos from '../../../services/pagos';
 import Button from "@mui/material/Button";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 import Modif from './modalactcomp';
+import Borrar from './modalborrar';
+
 export default function DetallesPagos(props) {
-    let params = useParams()
-    let id = params.id
-    const navigate = useNavigate();
+    let params = useParams();
+    let id = params.id;
     
+    const [pagos, setPagos] = useState([]);
+
     useEffect(() => {
-        console.log(id)
-        traer()
+        traer();
+    }, []);
 
-    }, [])
+    const traer = async () => {
+        const aux = { id: id };
+        const pag = await servicioPagos.detallesPagocli(aux);
+        setPagos(pag);
+    };
 
-
-  const [pagos, setPagos] = useState([]);
-
-  const traer = async () => {
-    const aux={
-        id:id
-    }
-  
-    const pag = await servicioPagos.detallesPagocli(aux)
-    
-    setPagos(pag)
-
-
-
-
-} 
-
-  const columns = [
-    {
-        name: "id",
-        label: "id",
-    },
-    {
-        name: "cuo",
-        label: "cuota",
-    },
-    {
-        name: "mes_anyo",
-        label: "mes",
-        
-        
-    },
-
- 
-     {
-        name: "cuil_cuit_administrador",
-        label: "Cuil administrador",
-    },
-    {
-        name: "Modificar",
-        options: {
-            customBodyRenderLite: (dataIndex, rowIndex) =>
-                modifa(
-                    dataIndex,
-                    rowIndex,
-                   // overbookingData,
-                   // handleEditOpen
-                )
+    const download = async (index) => {
+        try {
+            const pdfBlob = await servicioPagos.traerPdfConstanciadepago(pagos[index].id);
+            const url = URL.createObjectURL(pdfBlob);
+            window.open(url, '_blank');
+        } catch (error) {
+            console.error('Error al obtener el PDF:', error);
+            alert('Error al cargar el PDF');
         }
-    
- 
-    },
-    {
-        name: "Ver/borrar",
-        options: {
-            customBodyRenderLite: (dataIndex, rowIndex) =>
-            downloadFile(
-                    dataIndex,
-                    rowIndex,
-                   // overbookingData,
-                   // handleEditOpen
-                )
-        }
-    
-    },   
-   
-];
+    };
 
-async function download(index, rowIndex, data) {
-    try {
-        const pdfBlob = await servicioPagos.traerPdfConstanciadepago(pagos[index].id);
-        const url = URL.createObjectURL(pdfBlob);
-        window.open(url, '_blank');
-      } catch (error) {
-        console.error('Error al obtener el PDF:', error);
-        alert('Error al cargar el PDF');
-      }
-
- 
-}
-
-
-function modifa(index, rowIndex, data) {
-
-    /* const filename = (products[index].key)
-    console.log(filename)
-    const link = await axios.get(`http://localhost:4000/usuario1/get-object-url/` + filename)
-    console.log(link.data)
-    setAct(true) */
     return (
-        <>
-            
-   {pagos[index].ubicacion != null ? <>Tiene comprobante
-    <Modif
-id={pagos[index].id}/></>:<>
-    <Modif
-id={pagos[index].id}/>
-</>}
-   
-
-
-        </>
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>ID</TableCell>
+                        <TableCell>Cuota</TableCell>
+                        <TableCell>fecha</TableCell>
+                        <TableCell>Modificar</TableCell>
+                        <TableCell>Ver/Borrar</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {pagos.map((pago, index) => (
+                        <TableRow key={pago.id}>
+                            <TableCell>{pago.id}</TableCell>
+                            <TableCell>{pago.cuo}</TableCell>
+                            <TableCell>{pago.messs}/{pago.aniooo}</TableCell>
+                            <TableCell>
+                                {pago.ubicacion != null ? (
+                                    <>
+                                        Tiene comprobante
+                                        <Modif id={pago.id} />
+                                    </>
+                                ) : (
+                                    <Modif id={pago.id} />
+                                )}
+                            </TableCell>
+                            <TableCell>
+                                <Button onClick={() => download(index)}>Ver Online</Button>
+                                <Borrar id={pago.id} />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
-}
-function downloadFile(index, rowIndex, data) {
-
-    /* const filename = (products[index].key)
-    console.log(filename)
-    const link = await axios.get(`http://localhost:4000/usuario1/get-object-url/` + filename)
-    console.log(link.data)
-    setAct(true) */
-    return (
-        <>
-            
-              <Button
-                    onClick={() => download(index)}
-                >Ver Online</Button> 
-<Borrar
-id={pagos[index].id}/>
-
-        </>
-    );
-}
-return (
-
-    <MUIDataTable
-        title={"Lista de Pagos"}
-        data={pagos}
-        columns={columns}
-        actions={[
-            {
-                icon: 'save',
-                tooltip: 'Save User',
-                onClick: (event, rowData) => alert("You saved " + rowData.name)
-            }
-        ]}
-        />
-      
-)
 }
