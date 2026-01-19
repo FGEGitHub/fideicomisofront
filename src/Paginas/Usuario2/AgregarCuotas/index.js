14b01da0173b6535c7602d030875fe72d7c4510e
FGEGitHub <pipao.pipo@gmail.com>
2026-01-19 12:34:48 -0300

    ind

--- src/Paginas/Usuario2/AgregarCuotas/index.js
+++ src/Paginas/Usuario2/AgregarCuotas/index.js
@@ -266,7 +266,8 @@ export default function MenuUsuario2() {
                             <option value={'2022'}>2022</option>
                             <option value={'2023'}>2023</option>
                             <option value={'2024'}>2024</option>
-                            <option value={'2024'}>2025</option>
+                            <option value={'2025'}>2025</option>
+                              <option value={'2026'}>2026</option>
                           </NativeSelect>
                         </Box>
                       </Grid>
@@ -329,6 +330,7 @@ export default function MenuUsuario2() {
                             <option value={'2023'}>2023</option>
                             <option value={'2024'}>2024</option>
                             <option value={'2025'}>2025</option>
+                            <option value={'2026'}>2026</option>
                           </NativeSelect>
                         </Box>
                       </Grid>
