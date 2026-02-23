a6420e8ed32289cb4b66f4d86af089881248d7ac
FGEGitHub <pipao.pipo@gmail.com>
2026-02-23 12:19:19 -0300

    hj

--- src/services/url.js
+++ src/services/url.js
@@ -2,13 +2,13 @@
 module.exports = {
 
 
- database:'http://localhost:4000/'
+ //database:'http://localhost:4000/'
 //database: 'https://172.25.50.45:4000/'
  //database:'https://apifdd.ciudaddecorrientes.gov.ar/'
 
 
 
-//database:'https://api.santacatalinafideicomiso.com/'
+database:'https://api.santacatalinafideicomiso.com/'
 
 }
 ///const  baseUrl = '90.57.233.213:8084/'
